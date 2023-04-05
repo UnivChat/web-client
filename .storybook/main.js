2c51f2ea23d4f043d77b27b6ad0f4e117e0f8a74
minsoo kim <zlemzlem5656@naver.com>
2023-04-05 19:18:48 +0900

    chore: Storybook path alias config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,43 @@
+const path = require("path");
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
+  framework: "@storybook/react",
+  core: {
+    builder: "@storybook/builder-webpack5"
+  },
+  webpackFinal: config => {
+    // Default rule for images /\.(svg|ico|jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "~": path.resolve(__dirname, "../src/"),
+      "@svgs": path.resolve(__dirname, "../src/assets/svgs")
+    };
+
+    let loaders = config.resolve;
+    loaders.fallback = {
+      fs: false,
+      os: false,
+      path: false
+    };
+
+    const fileLoaderRule = config.module.rules.find(
+      rule => rule.test && rule.test.test(".svg")
+    );
+    fileLoaderRule.exclude = /\.svg$/;
+
+    config.module.rules.push({
+      test: /\.svg$/,
+      enforce: "pre",
+      loader: require.resolve("@svgr/webpack")
+    });
+
+    return config;
   }
-}
+};
