03be81f39f2db5cb607b2079a52dfe88399c838f
minsoo kim <zlemzlem5656@naver.com>
2023-04-05 20:31:30 +0900

    chore: #6 SVG를 추가합니다.

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,2 +1,8 @@
+export { default as chatActive } from "./chat-active.svg";
+export { default as chat } from "./chat.svg";
+export { default as cogActive } from "./cog-active.svg";
+export { default as cog } from "./cog.svg";
+export { default as homeActive } from "./home-active.svg";
+export { default as home } from "./home.svg";
 export { default as peoplePlus } from "./people-plus.svg";
 export { default as play } from "./play.svg";
