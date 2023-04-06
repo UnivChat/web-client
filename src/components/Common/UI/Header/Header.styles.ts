74a3703545531094ea85e983c6bb97157681b071
duck <godqhr2256@gmail.com>
2023-04-06 23:33:39 +0900

    feat: 헤더 스타일

---
+++ src/components/Common/UI/Header/Header.styles.ts
@@ -0,0 +1,27 @@
+import styled from "@emotion/styled";
+
+export const Title = styled.span`
+  font-family: "NanumSquare";
+  font-weight: 800;
+  font-size: 2.5vh;
+  letter-spacing: 0px;
+  color: #003091;
+  opacity: 1;
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 66.5px 51.5px 51.5px 20px;
+`;
+
+export const Button = styled.button`
+  border: none;
+  opacity: 1;
+  color: #003091;
+  font-family: "NanumSquare";
+  font-weight: 800;
+  padding-right: 20px;
+`;
