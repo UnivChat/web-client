4c6c73901727eea4f21b74853dbe137f9c905990
신재훈 <sjhh0105@naver.com>
2023-04-06 11:50:54 +0900

    feat: #4 타입수정

--- src/components/Common/UI/Svg/Svg.types.ts
+++ src/components/Common/UI/Svg/Svg.types.ts
@@ -3,7 +3,7 @@ import type * as svgList from "@svgs";
 export interface ContainerProps {
   _width?: number | `${number}%` | `${number}px` | string;
   _height?: number | `${number}%` | `${number}px` | string;
-  _fill?: Style.ColorKey | string;
+  _fill?: Style.ColorKey;
 }
 
 export interface SvgProps extends ContainerProps {
