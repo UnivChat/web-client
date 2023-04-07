b16e16d07596e28f6c4ef9ef65a26a6ac4b4b85f
신재훈 <sjhh0105@naver.com>
2023-04-07 22:19:50 +0900

    feat: #4 Svg emotion으로 바꿈

--- src/pages/login/index.page.tsx
+++ src/pages/login/index.page.tsx
@@ -1,14 +1,12 @@
 import React, { useCallback } from "react";
-import { Svg } from "~/components/Common";
 import * as Styled from "./loginPage.styles";
-import { useLoginForm } from './loginPage.hooks';
+import { useLoginForm } from "./loginPage.hooks";
 
 const Login = () => {
   const { email, password } = useLoginForm();
 
   const handleSubmit = useCallback((e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    
   }, []);
 
   return (
@@ -18,31 +16,13 @@ const Login = () => {
 
         <Styled.LoginForm onSubmit={handleSubmit}>
           <Styled.InputWrapper>
-            <Svg
-              svgName="enter"
-              _width="2vh"
-              _height="2vh"
-              _fill="background"
-            />
-            <Styled.IdBox
-              type="email"
-              {...email}
-              placeholder="아이디"
-            />
+            <Styled.IdPwIcon svgName="enter" />
+            <Styled.IdBox type="email" {...email} placeholder="아이디" />
           </Styled.InputWrapper>
 
           <Styled.InputWrapper>
-            <Svg
-              svgName="unlock"
-              _width="2vh"
-              _height="2vh"
-              _fill="background"
-            />
-            <Styled.PwBox
-              type="password"
-              {...password}
-              placeholder="비밀번호"
-            />
+            <Styled.IdPwIcon svgName="unlock" />
+            <Styled.PwBox type="password" {...password} placeholder="비밀번호" />
           </Styled.InputWrapper>
 
           <Styled.LoginButton>입장하기</Styled.LoginButton>
