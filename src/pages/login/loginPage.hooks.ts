6c763e7756f948b630ac519eaff468ca83a8af02
신재훈 <sjhh0105@naver.com>
2023-04-07 14:17:36 +0900

    feat: #4 hooks 생성

---
+++ src/pages/login/loginPage.hooks.ts
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+
+export const useLoginForm = () => {
+    const [loginFormData, setloginFormData] = useState<{
+        email: "",
+        password: ""
+      }>({
+        email: "",
+        password: "",
+      });
+    
+      const handleInputChange = (
+        event: React.ChangeEvent<HTMLInputElement>,
+      ) => {
+        setloginFormData(prevState => ({
+          ...prevState,
+          [event.target.type]: event.target.value
+        }));
+      };
+    
+
+  return {
+    email: {
+        value: loginFormData.email,
+        onChange: handleInputChange
+    },
+    password: {
+        value: loginFormData.password,
+        onChange: handleInputChange
+    }
+  }
+}
+
+export default useLoginForm;
