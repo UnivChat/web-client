c491df284e2e3b387ec98e5bd74914299138fa18
duck <godqhr2256@gmail.com>
2023-04-12 20:38:36 +0900

    feat: #9 뒤로가기 버튼 컴포넌트 수정

---
+++ src/components/Common/UI/Header/BackButton.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Svg } from "~/components/Common";
+import { useRouter } from "next/router";
+import { Button } from "./Header.styles";
+
+const BackButton = () => {
+  const router = useRouter();
+
+  return (
+    <Button
+      type="button"
+      onClick={() => {
+        router.back();
+      }}
+    >
+      <Svg _width={8} _height={16} svgName="back" />
+    </Button>
+  );
+};
+
+export default BackButton;
