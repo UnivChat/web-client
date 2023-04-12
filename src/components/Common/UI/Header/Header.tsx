4fce34e7452c47ed4f0632afe3086e8880c066a8
duck <godqhr2256@gmail.com>
2023-04-12 20:42:11 +0900

    feat: #9 헤더 컴포넌트 수정

--- src/components/Common/UI/Header/Header.tsx
+++ src/components/Common/UI/Header/Header.tsx
@@ -1,14 +1,11 @@
 import { Container, Title } from "./Header.styles";
 import type { HeaderProps } from "./Header.types";
-import LeftButton from "./LeftButton";
+import BackButton from "./BackButton";
 
-const Header = ({ title, location, leftButton }: HeaderProps) => {
-  const leftButtonType = leftButton || (location ? "back" : null);
-  /* 오른쪽 버튼(홈)으로 가기 위한 코드 */
-  // const rightButtonType = rightButton || (location ? "home" : null);
+const Header = ({ title, hasBackButton }: HeaderProps) => {
   return (
     <Container>
-      <LeftButton type={leftButtonType} />
+      {hasBackButton && <BackButton />}
       <Title>{title}</Title>
     </Container>
   );
