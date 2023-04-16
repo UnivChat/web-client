1ac5271bc6eadef824d0344e950541e9c6a45a69
신재훈 <sjhh0105@naver.com>
2023-04-16 20:10:06 +0900

    feat: svg파일 export

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -2,3 +2,8 @@ export { default as peoplePlus } from "./people-plus.svg";
 export { default as play } from "./play.svg";
 export { default as enter } from "./enter.svg";
 export { default as unlock } from "./unlock.svg";
+export { default as food } from "./food.svg";
+export { default as library } from "./library.svg";
+export { default as contact } from "./contact.svg";
+export { default as facility } from "./facility.svg";
+
