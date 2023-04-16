4ed56fb3809c6729e1e5c25ebd799e3c1be230c0
신재훈 <sjhh0105@naver.com>
2023-04-16 20:12:21 +0900

    feat: 편의시설

---
+++ src/components/Home/Amenity/index.page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import * as Styled from "./Amenity.style"
+import AmenityItem from "./AmenityItem";
+
+
+function Amenity() {
+  return (
+    <Styled.Container>
+      <Styled.Amenities>
+        <AmenityItem svgName="food" label="식단" />
+        <AmenityItem svgName="library" label="도서관자리" href="http://203.229.203.240/8080/Domian5_jythh.asp" />
+        <AmenityItem svgName="contact" label="연락망" />
+        <AmenityItem svgName="facility" label="편의시설" />
+      </Styled.Amenities>
+    </Styled.Container>
+  );
+}
+
+
+export default Amenity;
