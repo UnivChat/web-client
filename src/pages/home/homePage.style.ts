a50c1028515df8a60b64456fbbc7c7899dcb1944
신재훈 <sjhh0105@naver.com>
2023-04-16 20:11:15 +0900

    feat: 메인화면 스타일

---
+++ src/pages/home/homePage.style.ts
@@ -0,0 +1,16 @@
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+
+export const globalStyles = css`
+  body {
+    background-color: #f8f9fb;
+  }
+`;
+
+export const Title = styled.div`
+  font-family: 'NanumGothic', sans-serif;
+  font-weight: 600;
+  font-size: 2.5vh;
+  color: #003197;
+  margin: 3vh 0px 0px 5vw;
+`
