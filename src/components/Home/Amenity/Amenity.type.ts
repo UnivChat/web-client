bc5861caca2786b10c997e26936395313f7948d7
신재훈 <sjhh0105@naver.com>
2023-04-16 20:10:35 +0900

    feat: 편의시설 type

---
+++ src/components/Home/Amenity/Amenity.type.ts
@@ -0,0 +1,8 @@
+import type { SvgProps } from "~/components/Common/UI/Svg/Svg.types";
+
+// AmenityItem.tsx
+export interface AmenityItemProps {
+    svgName: SvgProps["svgName"];
+    label: string;
+    href?: string;
+  }
