63a871beb386815302504968b84f28e8240f7946
minsoo kim <zlemzlem5656@naver.com>
2023-04-18 00:54:33 +0900

    advise: 재훈님 코드 피드백

--- src/pages/auth/login/index.page.tsx
+++ src/pages/auth/login/index.page.tsx
@@ -12,39 +12,36 @@ const Login = () => {
 
   return (
     <Styled.Container>
-      <Styled.LoginBox>
-        <Styled.AppName>CAT CHAT</Styled.AppName>
+      <Styled.AppName>CAT CHAT</Styled.AppName>
 
-        <Styled.LoginForm onSubmit={handleSubmit}>
-          <Styled.InputWrapper>
-            <Styled.IdPwIcon svgName="enter" />
-            <Styled.IdBox
-              type="text" // email이 아니라 id로 입력 받기
-              {...id}
-              placeholder="아이디"
-              name="id"
-            />
-          </Styled.InputWrapper>
+      <Styled.LoginForm onSubmit={handleSubmit}>
+        <Styled.InputWrapper>
+          <Styled.IdPwIcon svgName="enter" />
+          <Styled.Input
+            type="text" // email이 아니라 id로 입력 받기
+            {...id}
+            placeholder="아이디"
+            name="id"
+          />
+        </Styled.InputWrapper>
 
-          <Styled.InputWrapper>
-            <Styled.IdPwIcon svgName="unlock" />
-            <Styled.PwBox
-              type="password"
-              {...password}
-              placeholder="비밀번호"
-              name="password"
-            />
-          </Styled.InputWrapper>
+        <Styled.InputWrapper>
+          <Styled.IdPwIcon svgName="unlock" />
+          <Styled.Input
+            type="password"
+            {...password}
+            placeholder="비밀번호"
+            name="password"
+          />
+        </Styled.InputWrapper>
 
-          <Styled.LoginButton>입장하기</Styled.LoginButton>
-        </Styled.LoginForm>
+        <Styled.LoginButton>입장하기</Styled.LoginButton>
+      </Styled.LoginForm>
 
-        <Styled.SubArea>
-          <Styled.FindPw href="">비밀번호 찾기</Styled.FindPw>
-          <Styled.SubText>|</Styled.SubText>
-          <Styled.SignIn href="">회원가입</Styled.SignIn>
-        </Styled.SubArea>
-      </Styled.LoginBox>
+      <Styled.SubArea>
+        <Styled.SubText href="">비밀번호 찾기</Styled.SubText>
+        <Styled.SubText href="">회원가입</Styled.SubText>
+      </Styled.SubArea>
     </Styled.Container>
   );
 };
--- src/pages/auth/login/loginPage.styles.ts
+++ src/pages/auth/login/loginPage.styles.ts
@@ -1,145 +1,109 @@
-import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 import Link from "next/link";
 import { Svg } from "~/components/Common";
-import type {
-  IdPwBoxProps,
-  LoginFormProps,
-  SubAreaProps
-} from "./loginPage.type";
+import { colors } from "~/constants";
+import { pxToRem } from "~/utils";
 
-// Color
-export const colors = {
-  background: "#003091",
-  primary: "#f8f9fb",
-  secondary: "#1360fc",
-  text: "white"
-};
-
-// Font
-export const sharedTextStyles = css`
-  font-family: "NanumGothic", sans-serif;
-  color: ${colors.text};
-`;
-
-// Component styles
 export const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
-  background-color: ${colors.background};
-  position: fixed;
-  display: flex;
-  justify-content: center;
-`;
-
-export const LoginBox = styled.div`
-  padding-top: 42vh;
   display: flex;
   flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+
+  padding-bottom: ${pxToRem(163)};
+  min-height: 100vh;
+
+  background-color: ${colors.background};
+  color: ${colors.white};
 `;
 
-export const AppName = styled.div`
-  ${sharedTextStyles};
-  font-weight: 600;
-  font-size: 4.5vh;
+export const AppName = styled.span`
+  font-weight: 800;
+  font-size: ${pxToRem(40)};
   text-align: center;
 `;
 
-export const LoginForm = styled.form<LoginFormProps>`
-  margin-top: 4.5vh;
-  text-align: center;
+export const LoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: ${pxToRem(20)};
+
+  width: ${pxToRem(210)};
+  margin-top: ${pxToRem(38)};
 `;
 
 export const InputWrapper = styled.div`
-  width: 66vw;
-  height: 5vh;
-  border-radius: 62px;
-  box-shadow: inset 0px -7px 13px #00000029, 0px 3px 10px #00000029;
-  background-color: ${colors.primary};
-  margin-top: 2.3vh;
   display: flex;
   align-items: center;
-  padding-left: 4vw;
-  border: 1px solid transparent; // 테두리 추가
+
+  position: relative;
+
+  border-radius: ${pxToRem(62)};
 `;
 
 export const IdPwIcon = styled(Svg)`
-  width: 2vh;
   fill: ${colors.background};
-
-  ${({ svgName }) => {
-    if (svgName === "enter") {
-      return css`
-        height: 1.8vh;
-      `;
-    }
-
-    if (svgName === "unlock") {
-      return css`
-        height: 2vh;
-      `;
-    }
-
-    return css`
-      width: 2vh;
-    `;
-  }}
+  position: absolute;
+  left: ${pxToRem(14.2)};
 `;
+IdPwIcon.defaultProps = {
+  _width: 12,
+  _height: 13
+};
 
-export const IdBox = styled.input<IdPwBoxProps>`
-  ${sharedTextStyles};
-  font-weight: 400;
-  font-size: 1.5vh;
-  padding-left: 2vw;
-  width: 80%;
-  background-color: transparent;
+export const Input = styled.input`
+  width: 100%;
+  height: ${pxToRem(35)};
+  padding-left: ${pxToRem(14.2 * 2 + 6)};
+
+  background-color: ${colors.white};
+
   color: ${colors.background};
+  border: 1px solid transparent;
+  box-shadow: inset 0px -7px 13px #00000029, 0px 3px 10px #00000029;
+  border-radius: ${pxToRem(62)};
 
   ::placeholder {
     color: ${colors.background};
   }
 
   &:focus {
-    outline: none; // 기존 테두리 제거
-    ~ ${InputWrapper} {
-      border-color: ${colors.primary}; // 포커스 시 InputWrapper 테두리 색상 변경
-    }
+    border-color: ${colors.blue13}; // 포커스 시 InputWrapper 테두리 색상 변경
   }
 `;
 
-export const PwBox = styled(IdBox)<IdPwBoxProps>``;
-
 export const LoginButton = styled.button`
-  ${sharedTextStyles};
-  font-weight: 600;
-  margin-top: 6.7vh;
-  width: 66vw;
-  height: 5vh;
+  width: 100%;
+  height: ${pxToRem(35)};
+  margin-top: ${pxToRem(20)};
+
   box-shadow: inset 0px -7px 13px #00000029, 0px 6px 10px #00000029;
-  background-color: ${colors.secondary};
-  border-radius: 62px;
-  font-size: 2.2vh;
+  background-color: ${colors.blue13};
+  border-radius: ${pxToRem(62)};
+
+  font-weight: 600;
+  font-size: ${pxToRem(16)};
 `;
 
 export const SubArea = styled.div`
-  ${sharedTextStyles};
+  display: flex;
+  justify-content: center;
+  gap: ${pxToRem(21)};
+  width: ${pxToRem(210)};
+
   font-weight: 300;
-  text-align: center;
-  margin-top: 7vh;
+  margin-top: ${pxToRem(26)};
 `;
 
-export const FindPw = styled(Link)<SubAreaProps>`
-  ${sharedTextStyles};
+export const SubText = styled(Link)`
+  position: relative;
   font-size: 1.5vh;
-`;
 
-export const SubText = styled.span`
-  margin: 0 6vw;
-  ${sharedTextStyles};
-  font-size: 1.5vh;
-`;
-
-export const SignIn = styled(FindPw)<SubAreaProps>`
-  ${sharedTextStyles};
-  font-size: 1.5vh;
+  & + &::before {
+    content: "|";
+    position: absolute;
+    top: 50%;
+    left: -${pxToRem(21 / 2)};
+    transform: translate(-100%, -50%);
+  }
 `;
