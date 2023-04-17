1b9c2cbd13e65e2b492512229c94ddd22419538c
duck <godqhr2256@gmail.com>
2023-04-17 20:48:19 +0900

    feat: #9 뒤로가기 컴포넌트 수정

--- src/components/Common/UI/Header/BackButton.tsx
+++ src/components/Common/UI/Header/BackButton.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { Svg } from "~/components/Common";
 import { useRouter } from "next/router";
-import { Button } from "./Header.styles";
+import * as Styled from "./Header.styles";
 
 export const BackButton = () => {
   const router = useRouter();
 
   return (
-    <Button
+    <Styled.Button
       type="button"
       onClick={() => {
         router.back();
       }}
     >
       <Svg _width={8} _height={16} svgName="back" />
-    </Button>
+    </Styled.Button>
   );
 };
