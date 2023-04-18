7c013c0978c698fb4bdac5323ece2bd22f42e430
신재훈 <sjhh0105@naver.com>
2023-04-19 03:57:25 +0900

    편의시설 추가

--- src/pages/home/index.page.tsx
+++ src/pages/home/index.page.tsx
@@ -1,7 +1,13 @@
 import type { NextPageWithLayout } from "../app.types";
+import Amenity from '../../components/Home/Amenity/index.page';
 
 const HomePage: NextPageWithLayout = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      HomePage
+      <Amenity />
+    </div>
+  )
 };
 
 export default HomePage;
