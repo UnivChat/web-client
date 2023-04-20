e60610becf76b4ffc65e3391385611624d21b498
신재훈 <sjhh0105@naver.com>
2023-04-21 00:00:35 +0900

    feat: #14 공통 컴포넌트 storybook

---
+++ src/components/Common/auth/auth.stories.tsx
@@ -0,0 +1,49 @@
+// Auth.stories.tsx
+import React from "react";
+import type { Meta, Story } from "@storybook/react";
+import {
+  Header,
+  BodyTopsDots,
+  BodyTop,
+  CustomInput,
+  CustomText,
+  WarningMessage,
+  SubmitButton
+} from "./auth";
+import { Body } from "./auth.styles";
+
+export default {
+  title: "Auth",
+} as Meta;
+
+// Body
+const BodyTemplate: Story = () => <Body />;
+export const BodyComponent = BodyTemplate.bind({});
+
+// Header
+const HeaderTemplate: Story = () => <Header />;
+export const HeaderComponent = HeaderTemplate.bind({});
+
+// 점
+const BodyTopsDotsTemplate: Story = () => <BodyTopsDots />;
+export const BodyTopsDotsComponent = BodyTopsDotsTemplate.bind({});
+
+// form 윗부분
+const BodyTopTemplate: Story = () => <BodyTop step={1} />;
+export const BodyTopComponent = BodyTopTemplate.bind({});
+
+// input title
+const CustomTextTemplate: Story = () => <CustomText>아아디</CustomText>;
+export const CustomTextComponent = CustomTextTemplate.bind({});
+
+// input
+const CustomInputTemplate: Story = () => <CustomInput type="text" name="example" required />;
+export const CustomInputComponent = CustomInputTemplate.bind({});
+
+// 경고 메세지
+const WarningMessageTemplate: Story = () => <WarningMessage>중복된 아이디입니다</WarningMessage>;
+export const WarningMessageComponent = WarningMessageTemplate.bind({});
+
+// 다음 버튼
+const SubmitButtonTemplate: Story = () => <SubmitButton>다음</SubmitButton>;
+export const SubmitButtonComponent = SubmitButtonTemplate.bind({});
