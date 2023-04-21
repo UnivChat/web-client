4f3d3ec8bb9cbf2ae3eb7bcb1c9029838f647d6c
duck <godqhr2256@gmail.com>
2023-04-21 22:27:55 +0900

    refactor: #9 BackButton 컴포넌트 리팩토링

--- src/components/Common/UI/Header/BackButton.tsx
+++ src/components/Common/UI/Header/BackButton.tsx
@@ -1,18 +1,17 @@
 import React from "react";
 import { Svg } from "~/components/Common";
 import { useRouter } from "next/router";
-import * as Styled from "./Header.styles";
+import * as Styled from "./BackButton.styles";
 
 export const BackButton = () => {
   const router = useRouter();
 
+  const handleBackClick = () => {
+    router.back();
+  };
+
   return (
-    <Styled.Button
-      type="button"
-      onClick={() => {
-        router.back();
-      }}
-    >
+    <Styled.Button type="button" onClick={handleBackClick}>
       <Svg _width={8} _height={16} svgName="back" />
     </Styled.Button>
   );
