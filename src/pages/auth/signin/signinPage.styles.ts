966b8586d3b04b57ca04bdb823cd72b7f3c24468
신재훈 <sjhh0105@naver.com>
2023-04-24 01:30:01 +0900

    feat: #14 불필요 스타일 제거

--- src/pages/auth/signin/signinPage.styles.ts
+++ src/pages/auth/signin/signinPage.styles.ts
@@ -2,10 +2,8 @@ import { colors } from "~/constants";
 import { pxToRem } from "~/utils";
 import styled from "@emotion/styled";
 
-
 export const BodyForm = styled.form`
   padding-top: ${pxToRem(22)};
-  padding-bottom: ${pxToRem(8)};
 `;
 
 export const BodyText = styled.div`
@@ -15,14 +13,13 @@ export const BodyText = styled.div`
   font-size: ${pxToRem(15)};
 `;
 
-
 export const CheckIdButton = styled.button`
   width: ${pxToRem(68)};
   height: ${pxToRem(33)};
   font-size: ${pxToRem(11)};
   font-weight: 600;
-  background-color: ${colors.background};
   color: ${colors.white};
+  background-color: ${colors.background};
   border: 1px solid;
   border-radius: ${pxToRem(50)};
   letter-spacing: ${pxToRem(0.7)};
@@ -37,9 +34,10 @@ export const GenderButton = styled.button<{ isSelected: boolean }>`
   font-weight: 600;
   width: ${pxToRem(68)};
   height: ${pxToRem(33)};
-  background-color: ${({ isSelected }) => (isSelected ? colors.background : '#fff')};
-  color: ${({ isSelected }) => (isSelected ? '#fff' : colors.background)};
-  border: 1px solid #D3D7E0;
+  background-color: ${({ isSelected }) =>
+    isSelected ? colors.background : "#fff"};
+  color: ${({ isSelected }) => (isSelected ? "#fff" : colors.background)};
+  border: 1px solid #d3d7e0;
   border-radius: ${pxToRem(50)};
   cursor: pointer;
   outline: none;
@@ -47,4 +45,4 @@ export const GenderButton = styled.button<{ isSelected: boolean }>`
   &:not(:last-child) {
     margin-right: ${pxToRem(12)};
   }
-`
+`;
