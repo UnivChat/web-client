17c4e117b0f5ee54070c0be1048218b5adb099c2
신재훈 <sjhh0105@naver.com>
2023-05-02 01:06:14 +0900

    feat: #14 공통스타일로 대체

--- src/pages/auth/emailauth/index.page.tsx
+++ src/pages/auth/emailauth/index.page.tsx
@@ -1,6 +1,12 @@
 import * as CommonComponents from "~/components/Common/auth/auth";
-import { Body, Container } from "~/components/Common/auth/auth.styles";
-import * as Styled from "./emailAuth.styles";
+import {
+  Body,
+  BodyForm,
+  CheckAuthButton,
+  Container,
+  Message,
+  Span
+} from "~/components/Common/auth/auth.styles";
 import { useEmailAuth } from "./emailAuth.hooks";
 
 const { Header, BodyTop, BodyTopsDots, CustomInput, CustomText, SubmitButton } =
@@ -19,30 +25,28 @@ const EmailAuth = () => {
       <Body>
         <BodyTop step={2} />
 
-        <Styled.BodyForm>
+        <BodyForm paddingTop={38} paddingBottom={8}>
           <CustomText>이메일을 입력해주세요</CustomText>
           <CustomInput type="email" name="email" required />
-        </Styled.BodyForm>
-        <Styled.CheckAuthButton
+        </BodyForm>
+        <CheckAuthButton
           style={{ backgroundColor: emailAuthButtonBgColor }}
           onClick={handleEmailAuthButtonClick}
         >
           이메일 주소를 인증합니다
-        </Styled.CheckAuthButton>
+        </CheckAuthButton>
         {isEmailAuthButtonClicked && (
           <>
-            <Styled.Message>
-              입력하신 이메일로 인증번호가 전송되었습니다
-            </Styled.Message>
-            <Styled.Span>
+            <Message>입력하신 이메일로 인증번호가 전송되었습니다</Message>
+            <Span>
               <BodyTopsDots />
-            </Styled.Span>
+            </Span>
 
-            <Styled.BodyForm>
+            <BodyForm paddingTop={38} paddingBottom={8}>
               <CustomText>인증번호를 입력해주세요</CustomText>
               <CustomInput type="text" name="emailAuth" required />
-            </Styled.BodyForm>
-            <Styled.CheckAuthButton>인증하기</Styled.CheckAuthButton>
+            </BodyForm>
+            <CheckAuthButton>인증하기</CheckAuthButton>
           </>
         )}
         <SubmitButton>시작하기</SubmitButton>
