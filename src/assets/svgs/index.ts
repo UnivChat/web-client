4922c7ed0d986444f5979a2c16e9c0a3785016b0
duck <godqhr2256@gmail.com>
2023-05-01 15:59:30 +0900

    feat: #24 검색 아이콘 svg 설정

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -8,3 +8,4 @@ export { default as peoplePlus } from "./people-plus.svg";
 export { default as play } from "./play.svg";
 export { default as enter } from "./enter.svg";
 export { default as unlock } from "./unlock.svg";
+export { default as blueSearch } from "./blue-search.svg";
