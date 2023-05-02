0e53bd73be6fcdc1b9a4fc35d87667e163de294b
duck <godqhr2256@gmail.com>
2023-05-02 23:21:11 +0900

    feat: #24 plus,minus svg 추가

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -9,3 +9,5 @@ export { default as play } from "./play.svg";
 export { default as enter } from "./enter.svg";
 export { default as unlock } from "./unlock.svg";
 export { default as blueSearch } from "./blue-search.svg";
+export { default as minus } from "./minus.svg";
+export { default as plus } from "./plus.svg";
