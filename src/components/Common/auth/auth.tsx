916d52af278fe91a25faf18571ee2cac89c7ebb0
신재훈 <sjhh0105@naver.com>
2023-05-20 16:26:10 +0900

    feat: #14 파일분리

--- src/components/Common/auth/auth.tsx
+++ src/components/Common/auth/auth.tsx
@@ -1,70 +1,8 @@
 import type { PropsWithChildren } from "react";
-import * as Styled from "./auth.styles";
+import * as Styled from "./Auth.styles";
 
 const Header = ({ text }: { text: string }) => {
   return <Styled.Header>{text}</Styled.Header>;
-};
-
-// ... 부분
-const BodyTopsDots = () => {
-  return (
-    <Styled.BodyTopDots>
-      <Styled.Dot>•</Styled.Dot>
-      <Styled.Dot>•</Styled.Dot>
-      <Styled.Dot>•</Styled.Dot>
-    </Styled.BodyTopDots>
-  );
-};
-
-// form 윗부분
-const BodyTop = ({ step }: { step: number }) => (
-  <Styled.BodyTop>
-    <Styled.BodyTopWrapper>
-      <Styled.BodyTopNumber opacity={step === 1 ? 1 : 0.6}>
-        1
-      </Styled.BodyTopNumber>
-      <BodyTopsDots />
-      <Styled.BodyTopNumber opacity={step === 2 ? 1 : 0.6}>
-        2
-      </Styled.BodyTopNumber>
-    </Styled.BodyTopWrapper>
-    <Styled.BodyTopText>
-      {step === 1 ? "기본 정보를 입력해주세요" : "학교 이메일을 인증해주세요"}
-    </Styled.BodyTopText>
-  </Styled.BodyTop>
-);
-
-// input
-const CustomInput = ({
-  type,
-  name,
-  required,
-  width,
-  ...props
-}: {
-  type: string;
-  name: string;
-  required: boolean;
-  width?: string;
-  props?: React.InputHTMLAttributes<HTMLInputElement>;
-}) => {
-  return (
-    <Styled.Input
-      type={type}
-      name={name}
-      required={required}
-      width={width}
-      {...props}
-    />
-  );
-};
-
-// input title
-const CustomText = ({
-  children,
-  marginTop
-}: PropsWithChildren<{ marginTop?: string }>) => {
-  return <Styled.Text marginTop={marginTop}>{children}</Styled.Text>;
 };
 
 // 경고 메세지
@@ -82,12 +20,4 @@ const SubmitButton = ({
   );
 };
 
-export {
-  Header,
-  BodyTopsDots,
-  BodyTop,
-  CustomInput,
-  CustomText,
-  WarningMessage,
-  SubmitButton
-};
+export { Header, WarningMessage, SubmitButton };
