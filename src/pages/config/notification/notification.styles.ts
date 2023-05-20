6eeda1ee96f04a00a0e66e654dba7373ad647ea2
신재훈 <sjhh0105@naver.com>
2023-05-20 22:36:29 +0900

    feat: #23 flex 수정, 개행

--- src/pages/config/notification/notification.styles.ts
+++ src/pages/config/notification/notification.styles.ts
@@ -1,39 +1,40 @@
 import styled from "@emotion/styled";
 import { colors } from "~/constants";
 import { pxToRem } from "~/utils";
+import { flex } from "~/styles/utils/flex";
 
 export const Container = styled.div`
-  position: fixed;
   min-height: 100vh;
   width: 100vw;
+
   background-color: ${colors.grayf8};
 `;
 
 export const Wrapper = styled.div<{ marginTop?: number }>`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0 ${pxToRem(37)};
+  ${flex({ justifyContent: "space-between", alignItems: "center" })}
+
   margin-top: ${({ marginTop }) => (marginTop ? pxToRem(marginTop) : "0")};
+  padding: 0 ${pxToRem(37)};
 `;
 
 export const Text = styled.span`
+  color: ${colors.background};
   font-size: ${pxToRem(14)};
   font-weight: 800;
-  color: ${colors.background};
 `;
 
-export const Switch = styled.label`
-  position: relative;
+export const Switch = styled.button`
   display: inline-block;
+  position: relative;
+
   width: ${pxToRem(48)};
   height: ${pxToRem(29)};
 `;
 
 export const SwitchInput = styled.input`
-  opacity: 0;
   width: 0;
   height: 0;
+  opacity: 0;
 
   &:checked + span {
     background-color: #003091;
@@ -54,22 +55,24 @@ export const SwitchInput = styled.input`
 
 export const SwitchSlider = styled.span`
   position: absolute;
-  cursor: pointer;
   top: 0;
   left: 0;
   right: 0;
   bottom: 0;
+
   background-color: #a2aab9;
-  transition: 0.4s;
   border-radius: ${pxToRem(34)};
+  transition: 0.4s;
 
   &:before {
     position: absolute;
     content: "";
+
     height: ${pxToRem(23)};
     width: ${pxToRem(23)};
     left: ${pxToRem(3)};
     bottom: ${pxToRem(3)};
+
     background-color: white;
     transition: 0.4s;
     border-radius: 50%;
