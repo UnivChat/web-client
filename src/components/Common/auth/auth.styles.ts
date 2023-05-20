61a905f49a53e5d857a956cc7e06291da534ae2c
신재훈 <sjhh0105@naver.com>
2023-05-20 16:25:58 +0900

    feat: #14 스타일 개행

--- src/components/Common/auth/auth.styles.ts
+++ src/components/Common/auth/auth.styles.ts
@@ -1,40 +1,33 @@
 import { colors } from "~/constants";
 import { pxToRem } from "~/utils";
 import styled from "@emotion/styled";
-
-function flex(
-  displayType = "",
-  flexDirection = "",
-  justifyContent = "",
-  alignItems = ""
-) {
-  return `
-    display: ${displayType};
-    ${flexDirection ? `flex-direction: ${flexDirection};` : ""}
-    ${justifyContent ? `justify-content: ${justifyContent};` : ""}
-    ${alignItems ? `align-items: ${alignItems};` : ""}
-  `;
-}
+import { flex } from "~/styles/utils/flex";
+import type { StyleProps } from "./Auth.type";
 
 export const Container = styled.div`
-  ${flex("flex", "column", "", "center")};
+  ${flex({ flexDirection: "column", alignItems: "center" })}
   position: fixed;
+
   min-height: 100vh;
+
   background-color: ${colors.white};
 `;
 
 export const Body = styled.div`
-  ${flex("flex", "column", "", "center")};
-  background-color: ${colors.grayf8};
-  margin-top: ${pxToRem(18)};
-  border-top-right-radius: ${pxToRem(40)};
+  ${flex({ flexDirection: "column", alignItems: "center" })}
+
   width: 100vw;
   height: 100vh;
+  margin-top: ${pxToRem(18)};
+
+  border-top-right-radius: ${pxToRem(40)};
+  background-color: ${colors.grayf8};
   box-shadow: inset 0 ${pxToRem(4)} ${pxToRem(24)} #00000019;
 `;
 
 export const Header = styled.span`
   padding-top: ${pxToRem(18)};
+
   color: ${colors.background};
   font-family: "NanumSquare", sans-serif;
   font-weight: 800;
@@ -46,59 +39,69 @@ export const BodyTop = styled.div`
   margin-top: ${pxToRem(30)};
 `;
 export const BodyTopWrapper = styled.div`
-  ${flex("flex", "", "", "center")};
+  ${flex({ alignItems: "center" })}
 `;
 
-export const BodyTopNumber = styled.span<{ opacity?: number }>`
-  ${flex("inline-flex", "", "center", "center")};
+export const BodyTopNumber = styled.span<StyleProps>`
+  display: inline-flex;
+  ${flex({ justifyContent: "center", alignItems: "center" })}
+
   width: ${pxToRem(20)};
   height: ${pxToRem(20)};
-  border-radius: ${pxToRem(40)};
-  border: 3px solid #003091;
+
   color: ${colors.background};
-  opacity: ${({ opacity }) => opacity || 1};
   font-weight: 800;
+  border: 3px solid #003091;
+  border-radius: ${pxToRem(40)};
+
+  opacity: ${({ opacity }) => opacity || 1};
 `;
 
 export const BodyTopDots = styled.span`
-  ${flex("inline-flex", "", "center", "center")};
+  display: inline-flex;
+  ${flex({ justifyContent: "center", alignItems: "center" })}
+
   margin: 0 2px;
 `;
 
 export const Dot = styled.span`
-  display: inline-block;
-  font-size: ${pxToRem(25)};
-  line-height: 1;
-  vertical-align: middle;
   position: relative;
+  vertical-align: middle;
+
   top: -1px;
-  opacity: 0.6;
-  color: #003091;
   margin: 0 -5px;
+
+  font-size: ${pxToRem(25)};
+  color: #003091;
+  opacity: 0.6;
 `;
 
-export const BodyTopText = styled.div<{ marginBottom?: number }>`
+export const BodyTopText = styled.div<StyleProps>`
   margin-top: ${pxToRem(15)};
-  font-size: ${pxToRem(15)};
-  font-weight: 800;
-  color: ${colors.background};
   margin-bottom: ${({ marginBottom }) =>
     marginBottom ? pxToRem(marginBottom) : "0"};
+
+  color: ${colors.background};
+  font-size: ${pxToRem(15)};
+  font-weight: 800;
 `;
 
-export const Text = styled.div<{ marginTop?: string }>`
-  margin-top: ${({ marginTop }) => marginTop || pxToRem(11)};
+export const Text = styled.div<StyleProps>`
+  margin-top: ${({ marginTop }) =>
+    marginTop ? pxToRem(marginTop) : pxToRem(11)};
   margin-left: ${pxToRem(10)};
   margin-bottom: ${pxToRem(8)};
+
+  color: ${colors.background};
   font-weight: 600;
   font-size: ${pxToRem(13)};
-  color: ${colors.background};
 `;
 
-export const Input = styled.input<{ width?: string }>`
-  padding-left: ${pxToRem(20)};
+export const Input = styled.input<StyleProps>`
   width: ${({ width }) => width || pxToRem(284)};
   height: ${pxToRem(35)};
+  padding-left: ${pxToRem(20)};
+
   background: #ffffff 0% 0% no-repeat padding-box;
   border: 0.8px solid #d3d7e0;
   border-radius: 50px;
@@ -108,56 +111,53 @@ export const Input = styled.input<{ width?: string }>`
 export const WarningMessage = styled.div`
   margin-left: ${pxToRem(10)};
   margin-top: ${pxToRem(8)};
+
   color: ${colors.warning};
   font-size: ${pxToRem(10)};
   font-weight: 600;
 `;
 export const SubmitButton = styled.button`
-  width: 100vw;
-  height: ${pxToRem(56)};
-  background-color: ${colors.background};
-  font-size: ${pxToRem(16)};
-  color: ${colors.white};
-  font-weight: 800;
-  letter-spacing: ${pxToRem(2)};
   position: fixed;
   left: 0;
   bottom: 0;
+
+  width: 100vw;
+  height: ${pxToRem(56)};
+
+  color: ${colors.white};
+  font-size: ${pxToRem(16)};
+  font-weight: 800;
+  letter-spacing: ${pxToRem(2)};
+  background-color: ${colors.background};
 `;
 
-export const BodyForm = styled.form<{
-  paddingTop?: number;
-  paddingBottom?: number;
-}>`
+export const BodyForm = styled.form<StyleProps>`
   padding-top: ${({ paddingTop }) => pxToRem(paddingTop || 0)};
   padding-bottom: ${({ paddingBottom }) => pxToRem(paddingBottom || 0)};
 `;
 
-export const CheckAuthButton = styled.button<{
-  marginTop?: number;
-}>`
+export const CheckAuthButton = styled.button<StyleProps>`
   width: ${pxToRem(231)};
   height: ${pxToRem(33)};
+  margin-top: ${({ marginTop }) => pxToRem(marginTop || 20)};
+
+  color: ${colors.white};
   font-size: ${pxToRem(11)};
   font-weight: 600;
-  background-color: ${colors.background};
-  color: ${colors.white};
+  letter-spacing: ${pxToRem(0.7)};
+  background-color: ${props => props.bgColor || colors.background};
   border: 1px solid;
   border-radius: ${pxToRem(50)};
-  letter-spacing: ${pxToRem(0.7)};
-  cursor: pointer;
-  outline: none;
-  margin-top: ${({ marginTop }) => pxToRem(marginTop || 20)};
 `;
 
 export const Span = styled.span`
-  margin-top: ${pxToRem(61)};
-  margin-bottom: ${pxToRem(20)};
+  margin: ${pxToRem(61)} 0 ${pxToRem(20)};
 `;
 
 export const Message = styled.div`
   margin-left: ${pxToRem(10)};
   margin-top: ${pxToRem(14)};
+
   color: ${colors.background};
   font-size: ${pxToRem(10)};
   font-weight: 600;
