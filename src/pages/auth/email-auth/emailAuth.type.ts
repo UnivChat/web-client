bdb97d2ae8e3c48adeb423f73b5292d1d2298a71
신재훈 <sjhh0105@naver.com>
2023-06-19 22:45:19 +0900

    feat: #30 이메일 api 타입 추가

--- src/pages/auth/email-auth/emailAuth.type.ts
+++ src/pages/auth/email-auth/emailAuth.type.ts
@@ -1,5 +1,8 @@
 export interface UseEmailAuthReturnValue {
+  email: string;
+  handleEmailChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  emailErrorMessage: string;
   isEmailAuthButtonClicked: boolean;
   emailAuthButtonBgColor: string;
-  handleEmailAuthButtonClick: VoidFunction;
+  handleEmailAuthButtonClick: () => Promise<void>;
 }
