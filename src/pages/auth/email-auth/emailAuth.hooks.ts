c25da3055ea284bab9b9188bff92488157d7b113
신재훈 <sjhh0105@naver.com>
2023-06-19 22:45:08 +0900

    feat: #30 이메일 api 타입 추가

--- src/pages/auth/email-auth/emailAuth.hooks.ts
+++ src/pages/auth/email-auth/emailAuth.hooks.ts
@@ -1,20 +1,45 @@
+import axios from "axios";
 import { useState } from "react";
+import type { ChangeEvent } from "react";
 import type { UseEmailAuthReturnValue } from "./emailAuth.type";
 
 export const useEmailAuth = (): UseEmailAuthReturnValue => {
+  const [email, setEmail] = useState<string>("");
+  const [emailErrorMessage, setEmailErrorMessage] = useState<string>("");
   const [isEmailAuthButtonClicked, setIsEmailAuthButtonClicked] =
-    useState(false);
+    useState<boolean>(false);
   const [emailAuthButtonBgColor, setEmailAuthButtonBgColor] =
-    useState("#003091");
+    useState<string>("#003091");
 
-  const handleEmailAuthButtonClick = () => {
-    setIsEmailAuthButtonClicked(true);
-    setEmailAuthButtonBgColor("#B3B8C1");
+  const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handleEmailAuthButtonClick = async () => {
+    if (!email || !/^\S+@\S+\.\S+$/.test(email)) {
+      setEmailErrorMessage("이메일 형식을 입력해주세요.");
+      return;
+    }
+    setEmailErrorMessage("");
+    try {
+      const response = await axios.post("/api/member/check/email", { email });
+      if (response.data.code === "1000") {
+        setIsEmailAuthButtonClicked(true);
+        setEmailAuthButtonBgColor("#B3B8C1");
+      } else {
+        setEmailErrorMessage("다시 이메일을 인증해주세요.");
+      }
+    } catch (error) {
+      setEmailErrorMessage("다시 이메일을 인증해주세요.");
+    }
   };
 
   return {
+    email,
+    handleEmailChange,
+    emailErrorMessage,
     isEmailAuthButtonClicked,
     emailAuthButtonBgColor,
-    handleEmailAuthButtonClick,
+    handleEmailAuthButtonClick
   };
 };
