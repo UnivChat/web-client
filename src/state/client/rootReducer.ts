46f2ce44e3b17407d72c927f602bf5cdca1caff3
신재훈 <sjhh0105@naver.com>
2023-07-09 12:46:37 +0900

    feat: email 상태관리

--- src/state/client/rootReducer.ts
+++ src/state/client/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import { rootReducer } from "./ducks";
+import emailReducer from "./changepw/emailSlice";
 
 const stores = combineReducers({
-  root: rootReducer
+  root: rootReducer,
+  email: emailReducer
 });
 
 export default stores;
