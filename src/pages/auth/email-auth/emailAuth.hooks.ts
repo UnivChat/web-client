b236fc60b3a26f50b7f11a2e5d72fc0356f620f8
신재훈 <sjhh0105@okpos.co.kr>
2023-07-26 09:45:32 +0900

    feat: api, mutation분리 / 리덕스 상태관리

--- src/pages/auth/email-auth/emailAuth.hooks.ts
+++ src/pages/auth/email-auth/emailAuth.hooks.ts
@@ -1,77 +1,65 @@
-import { useState } from "react";
-import { createAxiosInstance } from "@server-state/axios";
+import { useDispatch, useSelector } from "react-redux";
 import { useRouter } from "next/router";
-import { useMutation } from "@tanstack/react-query";
+import { useEmailVerification } from "@server-state/auth";
 import type { ChangeEvent } from "react";
+import type { RootState } from "@client-state/index";
 import type { UseEmailAuthReturnValue } from "./emailAuth.type";
+import {
+  setEmail,
+  setEmailAuth,
+  setEmailErrorMessage,
+  setAuthErrorMessage,
+  setIsVerified
+} from "../../../state/client/Auth/emailAuth/emailAuthSlice";
 
 export const useEmailAuth = (): UseEmailAuthReturnValue => {
-  const [email, setEmail] = useState("");
-  const [emailAuth, setEmailAuth] = useState("");
-  const [emailErrorMessage, setEmailErrorMessage] = useState("");
-  const [isEmailAuthButtonClicked, setIsEmailAuthButtonClicked] =
-    useState(false);
-  const [emailAuthButtonBgColor, setEmailAuthButtonBgColor] =
-    useState("#003091");
-  const [serverAuthCode, setServerAuthCode] = useState<number | null>(null);
-  const [authErrorMessage, setAuthErrorMessage] = useState("");
-  const [isVerified, setIsVerified] = useState(false);
-  const axiosInstance = createAxiosInstance({ needAuth: true });
+  const dispatch = useDispatch();
+  const {
+    email,
+    emailAuth,
+    emailErrorMessage,
+    isEmailAuthButtonClicked,
+    emailAuthButtonBgColor,
+    serverAuthCode,
+    authErrorMessage,
+    isVerified
+  } = useSelector((state: RootState) => state.emailAuth);
+
+  const verifyEmailMutation = useEmailVerification();
   const router = useRouter();
 
-  const mutation = useMutation(
-    (email: string) =>
-      axiosInstance
-        .post("/member/email/verified", { email })
-        .then(response => response.data),
-    {
-      onSuccess: data => {
-        if (data.code === "1000") {
-          setIsEmailAuthButtonClicked(true);
-          setEmailAuthButtonBgColor("#B3B8C1");
-          setServerAuthCode(data.result.checkCode);
-        } else {
-          setEmailErrorMessage("다시 이메일을 인증해주세요.");
-        }
-      },
-      onError: () => {
-        setEmailErrorMessage("다시 이메일을 인증해주세요.");
-      }
-    }
-  );
-
   const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
+    dispatch(setEmail(e.target.value));
   };
 
   const handleEmailAuthChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setEmailAuth(e.target.value);
-    setAuthErrorMessage("");
+    dispatch(setEmailAuth(e.target.value));
+    dispatch(setAuthErrorMessage(""));
   };
 
   const handleEmailAuthButtonClick = () => {
     if (!email || !/^\S+@\S+\.\S+$/.test(email)) {
-      setEmailErrorMessage("이메일 형식을 입력해주세요.");
+      dispatch(setEmailErrorMessage("이메일 형식을 입력해주세요."));
       return;
     }
-    setEmailErrorMessage("");
-    mutation.mutate(email);
+    dispatch(setEmailErrorMessage(""));
+    verifyEmailMutation.mutate(email);
   };
 
   const handleVerifyButtonClick = (): boolean => {
     if (!emailAuth) {
-      setAuthErrorMessage("인증번호를 입력해주세요.");
-      setIsVerified(false);
+      dispatch(setAuthErrorMessage("인증번호를 입력해주세요."));
+      dispatch(setIsVerified(false));
       return false;
     }
 
     const isCodeValid = String(serverAuthCode) === emailAuth;
     if (!isCodeValid) {
-      setAuthErrorMessage("인증번호가 틀립니다.");
-      setIsVerified(false);
+      dispatch(setAuthErrorMessage("인증번호가 틀립니다."));
+      dispatch(setIsVerified(false));
     } else {
-      setAuthErrorMessage("");
-      setIsVerified(true);
+      dispatch(setAuthErrorMessage(""));
+      dispatch(setIsVerified(true));
     }
     return isCodeValid;
   };
@@ -80,7 +68,7 @@ export const useEmailAuth = (): UseEmailAuthReturnValue => {
     if (isVerified) {
       router.push("/auth/sign-in");
     } else {
-      setAuthErrorMessage("인증번호를 입력해주세요.");
+      dispatch(setAuthErrorMessage("인증번호를 입력해주세요."));
     }
   };
 
