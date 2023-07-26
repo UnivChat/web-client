d43218515e33c1e45b4257b3ca5b0c3ce39c8927
신재훈 <sjhh0105@okpos.co.kr>
2023-07-26 09:46:22 +0900

    feat: 이메일 인증 mutation분리

--- src/state/server/auth/hooks/auth.mutations.ts
+++ src/state/server/auth/hooks/auth.mutations.ts
@@ -1,8 +1,15 @@
 import { useMutation } from "@tanstack/react-query";
+import { useDispatch } from "react-redux";
 import { deleteCookie, setCookie } from "cookies-next";
 import { useRouter } from "next/router";
 import { AC_TOKEN_KEY, RE_TOKEN_KEY } from "~/constants";
-import { signIn, signOut } from "../apis";
+import { signIn, signOut, verifyEmail } from "../apis";
+import {
+  setEmailErrorMessage,
+  setIsEmailAuthButtonClicked,
+  setEmailAuthButtonBgColor,
+  setServerAuthCode
+} from "../../../client/Auth/emailAuth/emailAuthSlice";
 
 export const useSignIn = () => {
   const { replace } = useRouter();
@@ -25,6 +32,25 @@ export const useSignOut = () => {
       deleteCookie(AC_TOKEN_KEY);
       deleteCookie(RE_TOKEN_KEY);
       push("/auth/sign-in");
+    }
+  });
+};
+
+export const useEmailVerification = () => {
+  const dispatch = useDispatch();
+
+  return useMutation(verifyEmail, {
+    onSuccess: data => {
+      if (data.code === "1000") {
+        dispatch(setIsEmailAuthButtonClicked(true));
+        dispatch(setEmailAuthButtonBgColor("#B3B8C1"));
+        dispatch(setServerAuthCode(data.result.checkCode));
+      } else {
+        dispatch(setEmailErrorMessage("다시 이메일을 인증해주세요."));
+      }
+    },
+    onError: () => {
+      dispatch(setEmailErrorMessage("다시 이메일을 인증해주세요."));
     }
   });
 };
