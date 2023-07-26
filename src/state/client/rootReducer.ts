e8029c52fc6d567c6947e07f7f19384b16ecb083
신재훈 <sjhh0105@okpos.co.kr>
2023-07-26 09:45:54 +0900

    feat: 이메일 인증 리덕스 추가

--- src/state/client/rootReducer.ts
+++ src/state/client/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import { rootReducer } from "./ducks";
-import emailReducer from "./changepw/emailSlice";
+import emailReducer from "./Auth/changepw/emailSlice";
+import emailAuthReducer from "./Auth/emailAuth/emailAuthSlice";
 
 const stores = combineReducers({
   root: rootReducer,
-  email: emailReducer
+  email: emailReducer,
+  emailAuth: emailAuthReducer
 });
 
 export default stores;
