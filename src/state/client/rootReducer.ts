3871cc6f309e17fc6f5fb3d134bb8222cf0fa13f
신재훈 <sjhh0105@okpos.co.kr>
2023-07-26 12:15:31 +0900

    feat: 리듀서 추가

--- src/state/client/rootReducer.ts
+++ src/state/client/rootReducer.ts
@@ -1,12 +1,16 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import { rootReducer } from "./ducks";
-import emailReducer from "./Auth/changepw/emailSlice";
+import emailReducer from "./Auth/find-pw/changepw/emailSlice";
 import emailAuthReducer from "./Auth/emailAuth/emailAuthSlice";
+import findPwReducer from "./Auth/find-pw/findPwSlice";
+import changePwReducer from "./Auth/find-pw/changepw/changePwSlice";
 
 const stores = combineReducers({
   root: rootReducer,
   email: emailReducer,
-  emailAuth: emailAuthReducer
+  emailAuth: emailAuthReducer,
+  findPw: findPwReducer,
+  changePw: changePwReducer
 });
 
 export default stores;
