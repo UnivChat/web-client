96de909766b963f05a8e813ed66bbfb944697959
신재훈 <sjhh0105@okpos.co.kr>
2023-07-26 12:14:35 +0900

    feat: 비밀번호 찾기 인증 api, mutation분리 및 엣지 케이스 전역 상태관리

--- src/pages/auth/find-pw/findPw.hooks.ts
+++ src/pages/auth/find-pw/findPw.hooks.ts
@@ -1,81 +1,66 @@
-import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { setEmail as setEmailInRedux } from "@client-state/Auth/changepw/emailSlice";
+import { setEmail as setEmailInRedux } from "@client-state/Auth/find-pw/changepw/emailSlice";
 import type { RootState } from "@client-state/index";
-import { createAxiosInstance } from "@server-state/axios";
 import { useRouter } from "next/router";
-import { useMutation } from "@tanstack/react-query";
-import type { AxiosResponseData, UseFindPwReturnValue } from "./findPw.type";
+import {
+  setEmailAuth,
+  setEmailErrorMessage,
+  setAuthErrorMessage,
+  setIsVerified
+} from "@client-state/Auth/find-pw/findPwSlice";
+import { useVerifyPasswordEmailMutation } from "@server-state/auth";
+import type { UseFindPwReturnValue } from "./findPw.type";
 
 export const useFindPw = (): UseFindPwReturnValue => {
-  const dispatch = useDispatch(); // Get dispatch function
-  const [emailAuth, setEmailAuth] = useState("");
-  const [emailErrorMessage, setEmailErrorMessage] = useState("");
-  const [isFindPwButtonClicked, setIsFindPwButtonClicked] = useState(false);
-  const [findPwButtonBgColor, setFindPwButtonBgColor] = useState("#003091");
-  const [serverAuthCode, setServerAuthCode] = useState<number | null>(null);
-  const [authErrorMessage, setAuthErrorMessage] = useState("");
-  const [isVerified, setIsVerified] = useState(false);
-  const axiosInstance = createAxiosInstance({ needAuth: true });
+  const dispatch = useDispatch();
   const router = useRouter();
+  const {
+    emailAuth,
+    emailErrorMessage,
+    isFindPwButtonClicked,
+    findPwButtonBgColor,
+    serverAuthCode,
+    authErrorMessage,
+    isVerified
+  } = useSelector((state: RootState) => state.findPw);
 
   const email = useSelector((state: RootState) => state.email);
-
-  const mutation = useMutation<AxiosResponseData, Error, string, unknown>(
-    email =>
-      axiosInstance
-        .post<AxiosResponseData>("/member/email/verified", { email })
-        .then(response => response.data),
-    {
-      onSuccess: data => {
-        if (data.code === "1000") {
-          setIsFindPwButtonClicked(true);
-          setFindPwButtonBgColor("#B3B8C1");
-          setServerAuthCode(data.result.checkCode);
-        } else {
-          setEmailErrorMessage("다시 이메일을 확인해주세요.");
-        }
-      },
-      onError: () => {
-        setEmailErrorMessage("다시 이메일을 확인해주세요.");
-      }
-    }
-  );
+  const mutation = useVerifyPasswordEmailMutation();
 
   const handleEmailChange: React.ChangeEventHandler<HTMLInputElement> = e => {
-    dispatch(setEmailInRedux(e.target.value)); // Dispatch setEmail action with new email
+    dispatch(setEmailInRedux(e.target.value));
   };
 
   const handleEmailAuthChange: React.ChangeEventHandler<
     HTMLInputElement
   > = e => {
-    setEmailAuth(e.target.value);
-    setAuthErrorMessage("");
+    dispatch(setEmailAuth(e.target.value));
+    dispatch(setAuthErrorMessage(""));
   };
 
   const handleFindPwButtonClick = () => {
     if (!email || !/^\S+@\S+\.\S+$/.test(email)) {
-      setEmailErrorMessage("이메일 형식을 확인해주세요.");
+      dispatch(setEmailErrorMessage("이메일 형식을 확인해주세요."));
       return;
     }
-    setEmailErrorMessage("");
+    dispatch(setEmailErrorMessage(""));
     mutation.mutate(email);
   };
 
   const handleVerifyButtonClick = (): boolean => {
     if (!emailAuth) {
-      setAuthErrorMessage("인증번호를 입력해주세요.");
-      setIsVerified(false);
+      dispatch(setAuthErrorMessage("인증번호를 입력해주세요."));
+      dispatch(setIsVerified(false));
       return false;
     }
 
     const isCodeValid = String(serverAuthCode) === emailAuth;
     if (!isCodeValid) {
-      setAuthErrorMessage("인증번호가 틀립니다.");
-      setIsVerified(false);
+      dispatch(setAuthErrorMessage("인증번호가 틀립니다."));
+      dispatch(setIsVerified(false));
     } else {
-      setAuthErrorMessage("");
-      setIsVerified(true);
+      dispatch(setAuthErrorMessage(""));
+      dispatch(setIsVerified(true));
       router.push("/auth/find-pw/changepw");
     }
     return isCodeValid;
@@ -85,7 +70,7 @@ export const useFindPw = (): UseFindPwReturnValue => {
     if (isVerified) {
       router.push("/auth/find-pw/changepw");
     } else {
-      setAuthErrorMessage("인증번호를 입력해주세요.");
+      dispatch(setAuthErrorMessage("인증번호를 입력해주세요."));
     }
   };
 
