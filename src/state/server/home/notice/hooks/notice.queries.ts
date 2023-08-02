9f3c5f541b0a4b2b2c7b2558995718c89739624f
신재훈 <sjhh0105@naver.com>
2023-08-02 23:50:13 +0900

    feat: 공지사항 query

---
+++ src/state/server/home/notice/hooks/notice.queries.ts
@@ -0,0 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
+import cheerio from "cheerio";
+import { fetchPosts } from "../api";
+
+export interface Post {
+  title: string;
+  link: string;
+}
+
+export const usePosts = (keyword: string) => {
+  return useQuery(["posts", keyword], () => parsePosts(fetchPosts(keyword)));
+
+  async function parsePosts(htmlPromise: Promise<string>) {
+    const html = await htmlPromise;
+    const $ = cheerio.load(html);
+    const $titleList = $(".rbbs_list_normal_sec > ul > li > a");
+
+    const titles: Post[] = [];
+    $titleList.each((idx, node) => {
+      const title = $(node).find(".text").text();
+      titles.push({
+        title: title.replace(/[\n\t새글]/g, ""),
+        link: `https://www.catholic.ac.kr${$(node).attr("href")}`
+      });
+    });
+
+    const filteredArr = titles.reduce((acc: Post[], current: Post) => {
+      const x = acc.find(item => item.title === current.title);
+      if (!x) {
+        return acc.concat([current]);
+      }
+      return acc;
+    }, []);
+
+    return filteredArr.slice(0, 6);
+  }
+};
