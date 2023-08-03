15e92c0eeb2fcc8138a2124798e0ab4ad2501c90
신재훈 <sjhh0105@okpos.co.kr>
2023-08-03 10:24:46 +0900

    feat: 리액트 쿼리 프리페치 수정

--- src/state/server/home/notice/hooks/notice.queries.ts
+++ src/state/server/home/notice/hooks/notice.queries.ts
@@ -11,22 +11,34 @@ export const usePosts = (keyword: string) => {
   const queryClient = useQueryClient();
 
   // 현재 페이지 데이터 가져오기
-  const queryResult = useQuery(["posts", keyword], () =>
-    parsePosts(fetchPosts(keyword))
+  const queryResult = useQuery(
+    ["posts", keyword],
+    () => parsePosts(fetchPosts(keyword)),
+    {
+      onSuccess: () => {
+        // 처음 데이터를 가져올 때만 나머지 탭도 데이터도 미리 가져옴
+        if (keyword === "1") {
+          prefetchNextPages(keyword, 3);
+        }
+      }
+    }
   );
 
-  // keyword를 기반으로 다음 키워드를 반환하는 함수
-  function getNextKeyword(keyword: string): string | null {
-    const nextKeyword = parseInt(keyword, 10) + 1;
-    return nextKeyword.toString();
+  function getNextKeywords(keyword: string, count: number): string[] {
+    return Array.from({ length: count }, (_, idx) =>
+      (parseInt(keyword, 10) + idx + 1).toString()
+    );
   }
 
-  // 다음 페이지 데이터 미리 가져오기
-  const nextKeyword = getNextKeyword(keyword);
-  if (nextKeyword) {
-    queryClient.prefetchQuery(["posts", nextKeyword], () =>
-      parsePosts(fetchPosts(nextKeyword))
-    );
+  function prefetchNextPages(keyword: string, count: number) {
+    const nextKeywords = getNextKeywords(keyword, count);
+    nextKeywords.forEach(nextKeyword => {
+      if (!queryClient.getQueryData(["posts", nextKeyword])) {
+        queryClient.prefetchQuery(["posts", nextKeyword], () =>
+          parsePosts(fetchPosts(nextKeyword))
+        );
+      }
+    });
   }
 
   return queryResult;
@@ -36,23 +48,17 @@ export const usePosts = (keyword: string) => {
     const $ = cheerio.load(html);
     const $titleList = $(".rbbs_list_normal_sec > ul > li > a");
 
-    const titles: Post[] = [];
+    const titlesSet: Set<Post> = new Set();
+
     $titleList.each((idx, node) => {
-      const title = $(node).find(".text").text();
-      titles.push({
-        title: title.replace(/[\n\t새글]/g, ""),
-        link: `https://www.catholic.ac.kr${$(node).attr("href")}`
-      });
+      const title = $(node)
+        .find(".text")
+        .text()
+        .replace(/[\n\t새글]/g, "");
+      const link = `https://www.catholic.ac.kr${$(node).attr("href")}`;
+      titlesSet.add({ title, link });
     });
 
-    const filteredArr = titles.reduce((acc: Post[], current: Post) => {
-      const x = acc.find(item => item.title === current.title);
-      if (!x) {
-        return acc.concat([current]);
-      }
-      return acc;
-    }, []);
-
-    return filteredArr.slice(0, 6);
+    return Array.from(titlesSet).slice(0, 6);
   }
 };
