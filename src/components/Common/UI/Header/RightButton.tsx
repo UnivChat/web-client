9ea4148340dcfbad55cc0727b8b7431b561039c6
duck <godqhr2256@gmail.com>
2023-08-06 21:59:21 +0900

    fix: 헤더 오른쪽 버튼 useRouter -> Link로 변경

--- src/components/Common/UI/Header/RightButton.tsx
+++ src/components/Common/UI/Header/RightButton.tsx
@@ -1,19 +1,17 @@
 import React from "react";
-import { useRouter } from "next/router";
 import { Svg } from "~/components/Common";
+import Link from "next/link";
 
 interface Props {
   rightButton?: string;
 }
 
 export const RightButton = ({ rightButton }: Props) => {
-  const { push } = useRouter();
-
   if (rightButton === "plus") {
     return (
-      <button type="button" onClick={() => push("/class/classSearch")}>
+      <Link href="class/classSearch">
         <Svg _width={15} _height={15} svgName="plus" />
-      </button>
+      </Link>
     );
   }
   return null;
