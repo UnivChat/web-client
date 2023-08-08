1c86576db1ac0cd2f25eed35eecae3c8b2d0a5d5
duck <godqhr2256@gmail.com>
2023-08-08 18:06:18 +0900

    refactor: 클래스 찾기, 클래스 채팅페이지 헤더 컴포넌트 적용 & 더미데이터 적용

--- src/pages/chat/class/classSearch/index.page.tsx
+++ src/pages/chat/class/classSearch/index.page.tsx
@@ -1,49 +1,54 @@
 import React from "react";
-import Header from "~/components/Common/UI/Header/Header";
+import { Header } from "~/components/Common/UI/Header/Header";
 import * as Styled from "./classSearch.styles";
 import { Search } from "./Search";
 import { ClassBox } from "./ClassBox";
 
+const dummyData = [
+  {
+    title: "[05421 - 2345] 금융시장의 이해",
+    subTitle: "화1 M403 / 목 1 -2 M403 | 분반02"
+  },
+  {
+    title: "[05421 - 2345] 금융시장의 이해",
+    subTitle: "화1 M403 / 목 1 -2 M403 | 분반02"
+  },
+  {
+    title: "[05421 - 2345] 금융시장의 이해",
+    subTitle: "화1 M403 / 목 1 -2 M403 | 분반02"
+  }
+];
+
 export default function ClassSearch() {
   return (
     <>
-      <Header title="클래스 찾기" hasBackButton />
+      <Header title="클래스 찾기" element={{ left: <Header.Back /> }} />
       <Styled.Container>
         <Search />
         <Styled.TitleContainer className="add-class">
           <Styled.Title>추가한 클래스</Styled.Title>
         </Styled.TitleContainer>
         <Styled.TitleDivider />
-        <ClassBox
-          svgName="minus"
-          title="[05421 - 2345] 금융시장의 이해"
-          subTitle="화1 M403 / 목 1 -2 M403 | 분반02"
-        />
-        <ClassBox
-          svgName="minus"
-          title="[05421 - 2345] 금융시장의 이해"
-          subTitle="화1 M403 / 목 1 -2 M403 | 분반02"
-        />
-        <ClassBox
-          svgName="minus"
-          title="[05421 - 2345] 금융시장의 이해"
-          subTitle="화1 M403 / 목 1 -2 M403 | 분반02"
-        />
-
+        {dummyData.map(data => (
+          <ClassBox
+            key={data.title}
+            title={data.title}
+            subTitle={data.subTitle}
+            svgName="minus"
+          />
+        ))}
         <Styled.TitleContainer className="class">
           <Styled.Title>클래스</Styled.Title>
         </Styled.TitleContainer>
         <Styled.TitleDivider />
-        <ClassBox
-          svgName="plus"
-          title="[05421 - 2345] 금융시장의 이해"
-          subTitle="화1 M403 / 목 1 -2 M403 | 분반02"
-        />
-        <ClassBox
-          svgName="plus"
-          title="[05421 - 2345] 금융시장의 이해"
-          subTitle="화1 M403 / 목 1 -2 M403 | 분반02"
-        />
+        {dummyData.map(data => (
+          <ClassBox
+            key={data.title}
+            title={data.title}
+            subTitle={data.subTitle}
+            svgName="plus"
+          />
+        ))}
       </Styled.Container>
     </>
   );
--- src/pages/chat/class/index.page.tsx
+++ src/pages/chat/class/index.page.tsx
@@ -1,32 +1,47 @@
 import React from "react";
-import Header from "~/components/Common/UI/Header/Header";
+import { Header } from "~/components/Common/UI/Header/Header";
 import type { NextPageWithLayout } from "../../app.types";
 import * as Styled from "./class.styles";
 import { ClassChatBox } from "./ClassChatBox";
 
+const dummyData = [
+  {
+    title: "일반수학1및연습 [분반]",
+    subTitle: "화 2~3(N301)",
+    chatCount: 5,
+    chatTime: "10:22"
+  },
+  {
+    title: "일반수학1및연습 [분반]",
+    subTitle: "화 2~3(N301)",
+    chatCount: 5,
+    chatTime: "10:22"
+  },
+  {
+    title: "일반수학1및연습 [분반]",
+    subTitle: "화 2~3(N301)",
+    chatCount: 5,
+    chatTime: "10:22"
+  }
+];
+
 const ClassChatPage: NextPageWithLayout = () => {
   return (
     <>
-      <Header title="클래스 채팅" hasBackButton rightButton="plus" />
+      <Header
+        title="클래스 채팅"
+        element={{ left: <Header.Back />, right: <Header.Plus /> }}
+      />
       <Styled.Container>
-        <ClassChatBox
-          title="일반수학1및연습 [분반]"
-          subTitle="화 2~3(N301)"
-          chatCount={5}
-          chatTime="10:22"
-        />
-        <ClassChatBox
-          title="일반수학1및연습 [분반]"
-          subTitle="화 2~3(N301)"
-          chatCount={5}
-          chatTime="10:22"
-        />
-        <ClassChatBox
-          title="일반수학1및연습 [분반]"
-          subTitle="화 2~3(N301)"
-          chatCount={5}
-          chatTime="10:22"
-        />
+        {dummyData.map(data => (
+          <ClassChatBox
+            key={data.title}
+            title={data.title}
+            subTitle={data.subTitle}
+            chatCount={data.chatCount}
+            chatTime={data.chatTime}
+          />
+        ))}
       </Styled.Container>
     </>
   );
