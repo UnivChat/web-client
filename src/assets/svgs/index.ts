136c58c87a37b18492c5d3104d494434e7d0f726
신재훈 <sjhh0105@naver.com>
2023-08-13 22:26:03 +0900

    feat: svg 추가

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -22,3 +22,5 @@ export { default as privateChat } from "./private-chat.svg";
 export { default as profile } from "./profile.svg";
 export { default as search } from "./search.svg";
 export { default as unlock } from "./unlock.svg";
+export { default as belowarrow } from "./belowarrow.svg";
+export { default as rightarrow } from "./rightarrow.svg";
