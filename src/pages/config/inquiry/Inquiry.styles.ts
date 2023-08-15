0dfdbd09cc24e88e0af84901928cbcd4e182098c
duck <godqhr2256@gmail.com>
2023-08-15 17:10:02 +0900

    refactor: 문의하기 스타일 수정 & 버튼 disabled 적용

--- src/pages/config/inquiry/Inquiry.styles.ts
+++ src/pages/config/inquiry/Inquiry.styles.ts
@@ -1,8 +1,9 @@
 import styled from "@emotion/styled";
 import { colors } from "~/constants/colors";
 import { pxToRem } from "~/utils";
+import { flex } from "~/styles/utils/flex";
 
-export const Container = styled.div`
+export const Container = styled.form`
   min-height: 100vh;
 
   background-color: ${colors.grayf8};
@@ -10,7 +11,7 @@ export const Container = styled.div`
 
 export const Title = styled.div`
   margin-top: ${pxToRem(32)};
-  margin-left: ${pxToRem(35)};
+  padding: 0 ${pxToRem(35)};
 
   font-family: "NanumSquare";
   font-weight: 800;
@@ -28,12 +29,25 @@ export const SubTitle = styled.div`
   font-family: "NanumSquare";
   font-weight: 600;
   font-size: ${pxToRem(13)};
+  color: #5c5c5c;
 `;
 
-export const Input = styled.input`
+export const ContentsInput = styled.textarea`
   margin-top: ${pxToRem(6)};
   width: 100%;
-  height: ${props => pxToRem(Number(props.height))};
+  height: ${pxToRem(175)};
+  resize: none;
+
+  background-color: #fcfdff;
+
+  border: ${pxToRem(1)} solid #d0d3d5;
+  border-radius: ${pxToRem(9)};
+`;
+
+export const EmailInput = styled.input`
+  margin-top: ${pxToRem(6)};
+  width: 100%;
+  height: ${pxToRem(30)};
 
   background-color: #fcfdff;
 
@@ -48,18 +62,27 @@ export const Title2 = styled.div`
   font-family: "NanumSquare";
   font-weight: 800;
   font-size: ${pxToRem(16)};
+  color: #5c5c5c;
+`;
+
+export const CheckContainer = styled.div`
+  ${flex({ alignItems: "center" })}
+
+  padding-left: ${pxToRem(15)};
+  column-gap: ${pxToRem(11)};
+  margin-top: ${pxToRem(18)};
 `;
 
 export const CheckButton = styled.input`
-  margin-top: ${pxToRem(18)};
-  margin-left: ${pxToRem(15)};
   width: ${pxToRem(17)};
   height: ${pxToRem(17)};
+
+  border-radius: ${pxToRem(4)};
+  /* border: 1px solid #d0d3d5; */
+  border: 1px solid red;
 `;
 
-export const CheckText = styled.span`
-  margin-left: ${pxToRem(11)};
-
+export const CheckText = styled.label`
   font-family: "NanumSquare";
   font-weight: 600;
   font-size: ${pxToRem(13)};
@@ -69,35 +92,31 @@ export const CheckText = styled.span`
 
 export const TextBox = styled.div`
   margin-top: ${pxToRem(16)};
-  width: 100%;
-  height: ${pxToRem(30)};
+  padding: ${pxToRem(7)} ${pxToRem(26)} ${pxToRem(5)};
 
   background-color: #f4f4f4;
-
-  border: ${pxToRem(1)} solid #d0d3d5;
-  border-radius: ${pxToRem(9)};
-`;
-
-export const Text = styled.div`
-  margin: ${pxToRem(7)} ${pxToRem(26)};
+  color: #5c5c5c;
 
   font-family: "NanumSquare";
   font-weight: 600;
   font-size: ${pxToRem(8)};
 
-  color: #5c5c5c;
+  border: ${pxToRem(1)} solid #d0d3d5;
+  border-radius: ${pxToRem(9)};
 `;
 
 export const Submit = styled.button`
   position: fixed;
   bottom: 0;
   width: 100%;
-  height: ${pxToRem(55)}; // TODO: 값 바꿔야 함
+
+  padding: ${pxToRem(20)} ${pxToRem(132)};
 
   font-family: "NanumSquare";
   font-weight: 800;
   font-size: ${pxToRem(17)};
 
-  color: #fafffd;
-  background-color: #003091;
+  color: ${({ disabled }) => (disabled ? "#fafffd" : "#fafffd")};
+  background-color: ${({ disabled }) => (disabled ? "#5c5c5c" : "#003091")};
+  opacity: ${({ disabled }) => (disabled ? 0.5 : 1)};
 `;
--- src/pages/config/inquiry/index.page.tsx
+++ src/pages/config/inquiry/index.page.tsx
@@ -1,32 +1,77 @@
+import { useForm } from "react-hook-form";
 import { Header } from "~/components/Common/UI/Header/Header";
 import type { NextPageWithLayout } from "~/pages/app.types";
+import { colors } from "~/constants";
 import * as Styled from "./Inquiry.styles";
+import type { InquiryProps } from "./Inquiry.types";
 
 const Inquiry: NextPageWithLayout = () => {
+  const { register, handleSubmit, watch } = useForm({
+    defaultValues: {
+      contents: "",
+      email: "",
+      privateCheck: false
+    }
+  });
+
+  const contents = watch("contents", "");
+  const email = watch("email", "");
+  const privateCheck = watch("privateCheck", false);
+
+  const isFormValid = () => {
+    const emailPattern = /^\S+@\S+$/i;
+    const isEmailValid = email && emailPattern.test(email);
+    const isContentsValid = contents.trim().length > 0;
+    const isPrivateCheckValid = privateCheck;
+
+    return isEmailValid && isContentsValid && isPrivateCheckValid;
+  };
+
+  // TODO: 문의 접수 API 연동
+  const onSubmit = (data: InquiryProps) => {
+    console.log(data, "data");
+  };
+
   return (
-    <Styled.Container>
-      <Header.Back title="문의하기" />
+    <Styled.Container onSubmit={handleSubmit(onSubmit)}>
+      <Header.Back bgColor={colors.white} title="문의하기" />
       <Styled.Title>문의 내용 작성</Styled.Title>
       <Styled.SubContainer>
         <Styled.SubTitle>내용</Styled.SubTitle>
-        <Styled.Input height="175" />
+        <Styled.ContentsInput {...register("contents", { required: true })} />
       </Styled.SubContainer>
       <Styled.SubContainer>
         <Styled.SubTitle>회답 받을 이메일</Styled.SubTitle>
-        <Styled.Input height="30" />
+        <Styled.EmailInput
+          type="text"
+          {...register("email", {
+            required: true,
+            pattern: /^\S+@\S+$/i
+          })}
+        />
       </Styled.SubContainer>
       <Styled.Title2>개인정보 수집 및 이용</Styled.Title2>
       <Styled.SubContainer>
-        <Styled.CheckButton type="checkbox" />
-        <Styled.CheckText>개인정보 수집 및 이용 동의(필수)</Styled.CheckText>
+        <Styled.CheckContainer>
+          <Styled.CheckButton
+            id="checkbox"
+            type="checkbox"
+            placeholder="privateCheck"
+            {...register("privateCheck", { required: true })}
+          />
+          <Styled.CheckText htmlFor="checkbox">
+            개인정보 수집 및 이용 동의(필수)
+          </Styled.CheckText>
+        </Styled.CheckContainer>
+
         <Styled.TextBox>
-          <Styled.Text>
-            문의처리를 위해 이메일, 회원정보, 기기정보, 문의내용에 포함된
-            개인정보를 수집하며, 개인정보처리방침에 따라 3년 후 파기됩니다.
-          </Styled.Text>
+          문의처리를 위해 이메일, 회원정보, 기기정보, 문의내용에 포함된
+          개인정보를 수집하며, 개인정보처리방침에 따라 3년 후 파기됩니다.
         </Styled.TextBox>
       </Styled.SubContainer>
-      <Styled.Submit>문의 접수하기</Styled.Submit>
+      <Styled.Submit type="submit" disabled={!isFormValid()}>
+        문의 접수하기
+      </Styled.Submit>
     </Styled.Container>
   );
 };
