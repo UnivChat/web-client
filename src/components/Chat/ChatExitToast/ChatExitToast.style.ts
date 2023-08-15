d7ee18def97afe6b3a0937b8ae00986c10959b47
신재훈 <sjhh0105@naver.com>
2023-08-15 11:39:44 +0900

    feat: 버튼, props 변경

--- src/components/Chat/ChatExitToast/ChatExitToast.style.ts
+++ src/components/Chat/ChatExitToast/ChatExitToast.style.ts
@@ -1,7 +1,6 @@
 import styled from "@emotion/styled";
 import { pxToRem } from "~/utils";
 import { flex } from "~/styles/utils/flex";
-import type { ButtonProps } from "../ChatToastCommon.style";
 
 export const SubText = styled.div`
   font-size: ${pxToRem(8)};
@@ -11,14 +10,4 @@ export const SubText = styled.div`
 export const ButtonWrap = styled.div`
   margin-top: ${pxToRem(17)};
   ${flex({ justifyContent: "center" })}
-`;
-
-export const ExitButton = styled.button<ButtonProps>`
-  width: ${pxToRem(75)};
-  height: ${pxToRem(25)};
-
-  color: ${({ color }) => color || "#fff"};
-  background-color: ${({ backgroundColor }) => backgroundColor || "#002f92"};
-  border-radius: ${pxToRem(13)};
-  border: 0;
 `;
--- src/components/Chat/ChatToastCommon.style.ts
+++ src/components/Chat/ChatToastCommon.style.ts
@@ -5,6 +5,8 @@ import { Svg } from "../Common";
 
 export type ButtonProps = {
   color?: string;
+  width?: string;
+  height?: string;
   backgroundColor?: string;
   marginBottom?: string;
 };
@@ -69,12 +71,31 @@ export const Text = styled.div<TextProps>`
   font-size: ${pxToRem(12)};
   font-weight: 600;
   letter-spacing: ${pxToRem(0.5)};
-  margin-bottom: ${({ marginBottom }) => marginBottom || pxToRem(12)};
+  margin-bottom: ${({ marginBottom = pxToRem(12) }) => marginBottom};
 `;
 export const TextHilight = styled.span`
   color: #002f92;
 `;
 
 export const Spacer = styled.div<SpacerProps>`
-  width: ${({ width }) => width || pxToRem(0)};
+  width: ${({ width }) => width};
+`;
+
+const CommonButtonStyle = styled.button<ButtonProps>`
+  width: ${({ width = pxToRem(190) }) => width};
+  height: ${({ height = pxToRem(21) }) => height};
+
+  color: ${({ color = "#fff" }) => color};
+  background-color: ${({ backgroundColor }) => backgroundColor || "#002f92"};
+  border-radius: ${pxToRem(13)};
+  border: 0;
+`;
+
+export const ExitButton = styled(CommonButtonStyle)`
+  width: ${pxToRem(75)};
+  height: ${pxToRem(25)};
+`;
+
+export const Button = styled(CommonButtonStyle)`
+  margin-bottom: ${({ marginBottom = pxToRem(7) }) => marginBottom};
 `;
