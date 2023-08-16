04fac471ca3ba4bed566c372ae80ab91c3e6373e
신재훈 <sjhh0105@okpos.co.kr>
2023-08-16 10:29:33 +0900

    fix: 개별 svg문제 해결

--- src/pages/home/contact/index.page.tsx
+++ src/pages/home/contact/index.page.tsx
@@ -1,79 +1,89 @@
 import React, { useState, useEffect } from "react";
-import { useContact } from "@server-state/home/notice/hooks/contact.queires";
+import { Header } from "~/components/Common/UI/Header/Header";
+import { useContact } from "@server-state/home/contact/hooks/contact.queires";
 import * as Styled from "./Contact.styles";
 import type { ContactDTO } from "./Contact.types";
 
 export const Contact = () => {
   const { data: contacts, isLoading, isError } = useContact();
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchContact, setSearchContact] = useState("");
   const [filteredContacts, setFilteredContacts] = useState<ContactDTO[]>([]);
-  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+  const [activeIndices, setActiveIndices] = useState<Record<number, boolean>>(
+    {}
+  );
 
   useEffect(() => {
     if (contacts) {
       setFilteredContacts(
         contacts.filter(contact =>
-          contact.major.toLowerCase().includes(searchTerm.toLowerCase())
+          contact.major.toLowerCase().includes(searchContact.toLowerCase())
         )
       );
     }
-  }, [contacts, searchTerm]);
+  }, [contacts, searchContact]);
+
+  const toggleContact = (index: number) => {
+    setActiveIndices({
+      ...activeIndices,
+      [index]: !activeIndices[index]
+    });
+  };
 
   return (
-    <Styled.Container>
-      <Styled.SearchContainer>
-        <Styled.SearchBox>
-          <Styled.SearchIcon svgName="search" />
-          <Styled.SearchInput
-            placeholder="필요한 연락망을 검색하세요"
-            value={searchTerm}
-            onChange={e => setSearchTerm(e.target.value)}
-          />
-        </Styled.SearchBox>
-      </Styled.SearchContainer>
-      <Styled.ContactContainer>
-        <Styled.ContactTitle>
-          <Styled.Title>전체연락망</Styled.Title>
-          <Styled.ContactBox>
-            {isLoading && (
-              <Styled.ErrorHandling>
-                연락망을 가지고 오고 있어요
-              </Styled.ErrorHandling>
-            )}
-            {isError && (
-              <Styled.ErrorHandling>
-                연락망을 가지고 오는데 실패했어요
-              </Styled.ErrorHandling>
-            )}
-            {filteredContacts.map((contact: ContactDTO, index: number) => (
-              <Styled.ContactList
-                // eslint-disable-next-line react/no-array-index-key
-                key={index}
-                onClick={() =>
-                  setActiveIndex(activeIndex === index ? null : index)
-                }
-              >
-                <Styled.ContactName>
-                  {contact.major}
-                  {activeIndex === index ? ( // 조건부 렌더링
-                    <Styled.ArrowIcon svgName="belowarrow" />
-                  ) : (
-                    <Styled.ArrowIcon svgName="rightarrow" />
-                  )}
-                </Styled.ContactName>
-                <Styled.ContactSub>
-                  {contact.phoneNumber
-                    ? `${contact.phoneNumber} (${contact.location})`
-                    : "정보가 없습니다"}
-                </Styled.ContactSub>
-
-                <Styled.ContactSub2>{contact.email}</Styled.ContactSub2>
-              </Styled.ContactList>
-            ))}
-          </Styled.ContactBox>
-        </Styled.ContactTitle>
-      </Styled.ContactContainer>
-    </Styled.Container>
+    <>
+      <Header.Back title="홈" />
+      <Styled.Container>
+        <Styled.SearchContainer>
+          <Styled.SearchBox>
+            <Styled.SearchIcon svgName="search" />
+            <Styled.SearchInput
+              placeholder="필요한 연락망을 검색하세요"
+              value={searchContact}
+              onChange={e => setSearchContact(e.target.value)}
+            />
+          </Styled.SearchBox>
+        </Styled.SearchContainer>
+        <Styled.ContactContainer>
+          <Styled.ContactTitle>
+            <Styled.Title>전체연락망</Styled.Title>
+            <Styled.ContactBox>
+              {isLoading && (
+                <Styled.ErrorHandling>
+                  연락망을 가지고 오고 있어요
+                </Styled.ErrorHandling>
+              )}
+              {isError && (
+                <Styled.ErrorHandling>
+                  연락망을 가지고 오는데 실패했어요
+                </Styled.ErrorHandling>
+              )}
+              {filteredContacts.map((contact, index) => (
+                <Styled.ContactList
+                  // eslint-disable-next-line react/no-array-index-key
+                  key={index}
+                  onClick={() => toggleContact(index)}
+                >
+                  <Styled.ContactName>
+                    {contact.major}
+                    <Styled.ArrowIcon
+                      svgName={
+                        activeIndices[index] ? "belowarrow" : "rightarrow"
+                      }
+                    />
+                  </Styled.ContactName>
+                  <Styled.ContactSub>
+                    {contact.phoneNumber
+                      ? `${contact.phoneNumber} (${contact.location})`
+                      : "정보가 없습니다"}
+                  </Styled.ContactSub>
+                  <Styled.ContactSub2>{contact.email}</Styled.ContactSub2>
+                </Styled.ContactList>
+              ))}
+            </Styled.ContactBox>
+          </Styled.ContactTitle>
+        </Styled.ContactContainer>
+      </Styled.Container>
+    </>
   );
 };
 
