d158d479fb994f339439ae8bd2eae4ce281c303e
신재훈 <sjhh0105@okpos.co.kr>
2023-08-16 10:28:26 +0900

    feat: 폴더구조 변경

--- src/state/server/home/notice/hooks/contact.queires.ts
+++
@@ -1,7 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import type { ContactDTO } from "~/pages/home/contact/Contact.types";
-import { fetchContact } from "../api";
-
-export const useContact = () => {
-  return useQuery<ContactDTO[]>(["contact"], fetchContact);
-};
