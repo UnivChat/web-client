57bd7fb418f0f28794ab90e448b884d5d37b9752
신재훈 <sjhh0105@naver.com>
2023-09-03 12:43:11 +0900

    feat: 닉네임 변경 퍼블리싱

--- src/pages/config/nickname/index.page.tsx
+++ src/pages/config/nickname/index.page.tsx
@@ -1,12 +1,30 @@
-import React from "react";
-import type { NextPageWithLayout } from "../../app.types";
+import { CustomInput, CustomText } from "~/components/Auth/Auth.Input";
+import {
+  BodyForm,
+  CheckAuthButton,
+  Container
+} from "~/components/Auth/Auth.styles";
+import type { NextPageWithLayout } from "~/pages/app.types";
+import { Header } from "~/components/Common/UI/Header/Header";
+import * as CommonStyled from "../config.styles";
 
 const NickName: NextPageWithLayout = () => {
-  return <div>nick</div>;
+  return (
+    <Container>
+      <Header.Back title="닉네임 변경" bgColor="white" />
+      <CommonStyled.ConfigBody>
+        <BodyForm paddingTop={70} paddingBottom={8}>
+          <CustomText>닉네임 변경하기</CustomText>
+          <CustomInput name="nickname" required />
+        </BodyForm>
+        <CheckAuthButton>확인</CheckAuthButton>
+      </CommonStyled.ConfigBody>
+    </Container>
+  );
 };
 
 export default NickName;
 
 NickName.layoutConfig = {
-  case: "tab"
+  case: "default"
 };
