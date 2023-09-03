b8368704cc2af434b23aebd5cb3b0af90c2bc7ce
신재훈 <sjhh0105@naver.com>
2023-09-03 12:42:53 +0900

    feat: 회원탈퇴 퍼블리싱

--- src/pages/config/deleteacc/index.page.tsx
+++ src/pages/config/deleteacc/index.page.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+import { CustomInput, CustomText } from "~/components/Auth/Auth.Input";
+import {
+  BodyForm,
+  CheckAuthButton,
+  Container
+} from "~/components/Auth/Auth.styles";
+import { Header } from "~/components/Common/UI/Header/Header";
+import * as CommonStyled from "../config.styles";
 import type { NextPageWithLayout } from "../../app.types";
 
 const DeleteAcc: NextPageWithLayout = () => {
-  return <div>DeleteAcc</div>;
+  return (
+    <Container>
+      <Header.Back title="회원탈퇴" bgColor="white" />
+      <CommonStyled.ConfigBody>
+        <BodyForm paddingTop={70} paddingBottom={8}>
+          <CustomText>계정 비밀번호</CustomText>
+          <CustomInput name="password" required />
+        </BodyForm>
+        <CheckAuthButton>확인</CheckAuthButton>
+      </CommonStyled.ConfigBody>
+    </Container>
+  );
 };
 
 export default DeleteAcc;
