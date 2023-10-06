e7e77494ac2b64278d93d18787d4069d2dcc3081
신재훈 <sjhh0105@okpos.co.kr>
2023-10-06 11:45:58 +0900

    공지사항 화살표 svg추가

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -53,3 +53,5 @@ export { default as team } from "./team.svg";
 export { default as toastexit } from "./toastexit.svg";
 export { default as toasttalk } from "./toasttalk.svg";
 export { default as unlock } from "./unlock.svg";
+export { default as left } from "./chevron-left.svg";
+export { default as right } from "./chevron-right.svg";
