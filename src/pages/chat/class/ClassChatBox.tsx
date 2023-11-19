1cbd6dd61e863f440e46d648f0ac8c290894acf4
신재훈 <sjhh0105@naver.com>
2023-11-19 23:28:29 +0900

    날짜 포멧팅(사파리 안보이는 이슈 남아있음)

--- src/pages/chat/class/ClassChatBox.tsx
+++ src/pages/chat/class/ClassChatBox.tsx
@@ -11,6 +11,41 @@ export const ClassChatBox = ({
 }: ClassChatBoxProps) => {
   const { push } = useRouter();
 
+  // TODO: 크롬에서만 시간이 보임 사파리에서 안보임
+  const safeDate = dateString => {
+    const date = new Date(dateString);
+    return isNaN(date.getTime()) ? null : date;
+  };
+
+  const chatDate = safeDate(chatTime);
+  const currentDate = new Date();
+
+  const formatDate = date => {
+    if (!date) return "";
+
+    const isToday =
+      date.getDate() === currentDate.getDate() &&
+      date.getMonth() === currentDate.getMonth() &&
+      date.getFullYear() === currentDate.getFullYear();
+    const isYesterdayOrBefore =
+      currentDate.getTime() - date.getTime() > 24 * 60 * 60 * 1000;
+
+    if (isToday) {
+      // 시간만 반환
+      return new Intl.DateTimeFormat("ko-KR", {
+        hour: "2-digit",
+        minute: "2-digit"
+      }).format(date);
+    } else if (isYesterdayOrBefore) {
+      // 어제 이후 이전 날짜들은 날짜만 반환
+      return new Intl.DateTimeFormat("ko-KR", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit"
+      }).format(date);
+    }
+  };
+
   return (
     <Styled.ChatBox
       onClick={() =>
@@ -22,13 +57,15 @@ export const ClassChatBox = ({
     >
       <div>
         <Styled.Title>{title}</Styled.Title>
-        <Styled.ChatTime>{chatTime}</Styled.ChatTime>
+        <Styled.ChatTime>{formatDate(chatDate)}</Styled.ChatTime>
       </div>
       <div>
         <Styled.SubTitle>{classTime}</Styled.SubTitle>
-        <Styled.ChatCountContainer>
-          <Styled.ChatCount>{chatCount}</Styled.ChatCount>
-        </Styled.ChatCountContainer>
+        {chatCount !== 0 ? (
+          <Styled.ChatCountContainer>
+            <Styled.ChatCount>{chatCount}</Styled.ChatCount>
+          </Styled.ChatCountContainer>
+        ) : null}
       </div>
     </Styled.ChatBox>
   );
